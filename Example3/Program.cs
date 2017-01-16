ab6cc8efb70922ea420eac241bc24a249e58f9a4
Beisenbek <beysenbek@gmail.com>
2017-01-16 12:51:17 +0600

    1

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Example3
 {
+
     class Program
     {
         /// <summary>
---
+++ Example4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example4
+{
+    public class Student
+    {
+        public string name;
+        public string surname;
+        public int age;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.name = "111";
+        }
+    }
+}
