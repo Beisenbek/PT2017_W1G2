d4f84b8c00129fc740e7ad41549c985f3c41afdf
Beisenbek <beysenbek@gmail.com>
2017-01-16 12:09:43 +0600

    initial

---
+++ Example2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            string[] param = line.Split(' ');
+            if (param.Length > 1)
+            {
+                int a = int.Parse(param[0]);
+                int b = int.Parse(param[1]);
+                Console.WriteLine(a + b);
+            }
+            else
+            {
+                int a = int.Parse(line);
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine(a + b);
+            }
+        }
+    }
+}
---
+++ Example3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example3
+{
+    class Program
+    {
+        static void Main(string[] x)
+        {
+            if(x.Length > 0)
+            {
+                if (x[0] == "--help")
+                {
+                    Console.WriteLine("Enter two numbers:");
+                }
+                else
+                {
+                    int a = int.Parse(x[0]);
+                    int b = int.Parse(x[1]);
+                    Console.WriteLine(a + b);
+                }
+            }
+            else
+            {
+                string line = Console.ReadLine();
+                string[] param = line.Split(' ');
+                if (param.Length > 1)
+                {
+                    int a = int.Parse(param[0]);
+                    int b = int.Parse(param[1]);
+                    Console.WriteLine(a + b);
+                }
+                else
+                {
+                    int a = int.Parse(line);
+                    int b = int.Parse(Console.ReadLine());
+                    Console.WriteLine(a + b);
+                }
+            }
+        }
+    }
+}
